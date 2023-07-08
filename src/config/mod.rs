c5ab3b934488ad92dadb4bed87999518d83f5931
raffaele-romeo <raffaele.romeo.91.rr@gmail.com>
2023-07-08 16:57:46 +0100

    Add iterators and closure to the solution

--- src/config/mod.rs
+++ src/config/mod.rs
@@ -7,13 +7,17 @@ pub struct Config {
 }
 
 impl Config {
-    pub fn build(args: &[String]) -> Result<Config, &str> {
-        if args.len() < 3 {
-            return Err("Not enough arguments");
-        }
+    pub fn build(mut args: impl Iterator<Item = String>) -> Result<Config, &'static str> {
+        args.next();
 
-        let query = args[1].clone();
-        let file_path = args[2].clone();
+        let query = match args.next(){
+            Some(value) => value,
+            None => return Err("Query param is missing")
+        };
+        let file_path = match args.next() {
+            Some(value) => value,
+            None => return Err("File path is missing")
+        };
         let ignore_case = env::var("IGNORE_CASE").is_ok();
 
         Ok(Config { query, file_path, ignore_case })
